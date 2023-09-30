9f136ab07edb52171240a23a9d98bc8591064ab5
Andrés Poveda <81783473+apovedq@users.noreply.github.com>
2023-09-30 16:48:40 -0500

    Add landing and login interaction

---
+++ app/src/main/java/com/example/firebase_config/SignInActivity.kt
@@ -0,0 +1,11 @@
+package com.example.firebase_config
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
