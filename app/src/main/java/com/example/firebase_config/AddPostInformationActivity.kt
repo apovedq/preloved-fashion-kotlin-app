a3278cde669ac8d724b5338cf53de106a07ede10
Keren López <kerenlopezcordoba@outlook.es>
2023-10-01 18:50:10 -0500

    feat: xml of add post information activity was created

---
+++ app/src/main/java/com/example/firebase_config/AddPostInformationActivity.kt
@@ -0,0 +1,11 @@
+package com.example.firebase_config
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddPostInformationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_post_information)
+    }
+}
