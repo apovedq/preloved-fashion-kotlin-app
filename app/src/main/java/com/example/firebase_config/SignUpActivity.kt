d3e12201076dc4a479ab44ebb695f06cabac2b3b
Laura-Martinez182 <71099708+Laura-Martinez182@users.noreply.github.com>
2023-10-19 23:25:21 -0500

    Facebook authentication is working. Username set up activity added

--- app/src/main/java/com/example/firebase_config/SignUpActivity.kt
+++ app/src/main/java/com/example/firebase_config/SignUpActivity.kt
@@ -1,15 +1,23 @@
 package com.example.firebase_config
 
+import android.R
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.firebase_config.databinding.ActivitySignUpBinding
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.FacebookSdk
+import com.facebook.login.LoginResult
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.ktx.auth
-import com.google.firebase.firestore.auth.User
-import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+
 
 class SignUpActivity : AppCompatActivity() {
 
@@ -17,8 +25,11 @@ class SignUpActivity : AppCompatActivity() {
         ActivitySignUpBinding.inflate(layoutInflater)
     }
 
+    val callbackManager = CallbackManager.Factory.create()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(binding.root)
 
         binding.registerBtn.setOnClickListener {
@@ -34,5 +45,53 @@ class SignUpActivity : AppCompatActivity() {
                 Toast.makeText(this, it.message, Toast.LENGTH_LONG).show()
             }
         }
+
+
+
+        binding.facebookBtn.setPermissions("email", "public_profile")
+        binding.facebookBtn.registerCallback(
+            callbackManager,
+            object : FacebookCallback<LoginResult> {
+                override fun onSuccess(loginResult: LoginResult) {
+                    Log.d(TAG, "facebook:onSuccess:$loginResult")
+                    handleFacebookAccessToken(loginResult.accessToken)
+                }
+
+                override fun onCancel() {
+                    Log.d(TAG, "facebook:onCancel")
+                }
+
+                override fun onError(error: FacebookException) {
+                    Log.d(TAG, "facebook:onError", error)
+                }
+            },
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+    }
+
+    private fun handleFacebookAccessToken(token: AccessToken) {
+        Log.d(TAG, "handleFacebookAccessToken:$token")
+
+        val credential = FacebookAuthProvider.getCredential(token.token)
+        Firebase.auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user = Firebase.auth.currentUser
+                    startActivity(Intent(this, MainHomePageActivity::class.java))
+                    finish()
+                } else {
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    Toast.makeText(
+                        this,
+                        "Error en la autenticación",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                }
+            }
     }
 }
