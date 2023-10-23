c83a304081a55c3b268987c99800995827ac834b
Andrés Poveda <81783473+apovedq@users.noreply.github.com>
2023-10-23 14:48:13 -0500

    fixed deprecated function in SplashActivity

--- app/src/main/java/com/example/firebase_config/SplashActivity.kt
+++ app/src/main/java/com/example/firebase_config/SplashActivity.kt
@@ -4,15 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import androidx.core.view.ViewCompat
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Handler().postDelayed({
-            startActivity(Intent(this, MainActivity::class.java))
+        lifecycleScope.launch {
+            delay(3000)
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
             finish()
-        }, 3000)
+        }
     }
 }
