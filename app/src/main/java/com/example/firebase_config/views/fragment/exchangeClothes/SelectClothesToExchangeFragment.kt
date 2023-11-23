603bbab22a253eb6a595e885871355659540b193
Keren López <kerenlopezcordoba@outlook.es>
2023-11-23 00:44:54 -0500

    Downloading posts of a specific user

--- app/src/main/java/com/example/firebase_config/views/fragment/exchangeClothes/SelectClothesToExchangeFragment.kt
+++ app/src/main/java/com/example/firebase_config/views/fragment/exchangeClothes/SelectClothesToExchangeFragment.kt
@@ -1,16 +1,22 @@
 package com.example.firebase_config.views.fragment.exchangeClothes
 
+import PostAdapterFeed
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.example.firebase_config.databinding.SelectClothesToExchangeFragmentBinding
+import com.example.firebase_config.viewmodels.ExchangeViewModel
 
 class SelectClothesToExchangeFragment: Fragment() {
 
+    private val viewModel: ExchangeViewModel by activityViewModels()
     private lateinit var binding: SelectClothesToExchangeFragmentBinding
 
+    @SuppressLint("NotifyDataSetChanged")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -19,7 +25,17 @@ class SelectClothesToExchangeFragment: Fragment() {
 
         binding = SelectClothesToExchangeFragmentBinding.inflate(inflater, container, false)
 
-        return super.onCreateView(inflater, container, savedInstanceState)
+        viewModel.downloadPosts()
+
+        viewModel.userPosts.observe(viewLifecycleOwner){posts ->
+            val adapter = PostAdapterFeed(posts)
+            binding.exchangeRecyclerView.adapter = adapter
+            adapter.notifyDataSetChanged()
+        }
+
+        binding.fpRequiredTV.text = viewModel.getFashionPoints().toString()
+
+        return binding.root
     }
 
     companion object{
