ceee590d7a99dfda394d7fe2e283ae1674ecea86
Keren López <kerenlopezcordoba@outlook.es>
2023-11-23 00:40:58 -0500

    Using fragments in ExchangeClothesActivity

--- app/src/main/java/com/example/firebase_config/views/ExchangeClothesActivity.kt
+++ app/src/main/java/com/example/firebase_config/views/ExchangeClothesActivity.kt
@@ -5,11 +5,31 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.firebase_config.R
 import com.example.firebase_config.databinding.ActivityExchangeClothesBinding
+import com.example.firebase_config.views.fragment.exchangeClothes.HowExchangeWorksFragment
+import com.example.firebase_config.views.fragment.exchangeClothes.NoClothesToExchangeFragment
+import com.example.firebase_config.views.fragment.exchangeClothes.SelectClothesToExchangeFragment
+import com.example.firebase_config.views.fragment.exchangeClothes.SuccessfulExchangeFragment
 
 class ExchangeClothesActivity : AppCompatActivity() {
 
     private val binding: ActivityExchangeClothesBinding by lazy {
         ActivityExchangeClothesBinding.inflate(layoutInflater)
+    }
+
+    val selectClothesToExchangeFragment by lazy {
+        SelectClothesToExchangeFragment.newInstance()
+    }
+
+    val successfulExchangeFragment by lazy {
+        SuccessfulExchangeFragment.newInstance()
+    }
+
+    val noClothesToExchangeFragment by lazy {
+        NoClothesToExchangeFragment.newInstance()
+    }
+
+    val howExchangeWorksFragment by lazy {
+        HowExchangeWorksFragment.newInstance()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
