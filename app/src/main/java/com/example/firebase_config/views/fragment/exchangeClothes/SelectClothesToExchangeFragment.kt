8e6797b6702f2dad0852213a1453ad50c313887f
Keren López <kerenlopezcordoba@outlook.es>
2023-11-25 18:39:55 -0500

    Connecting noClothesToExchangeFragment and howExchangeWorksFragment

--- app/src/main/java/com/example/firebase_config/views/fragment/exchangeClothes/SelectClothesToExchangeFragment.kt
+++ app/src/main/java/com/example/firebase_config/views/fragment/exchangeClothes/SelectClothesToExchangeFragment.kt
@@ -10,6 +10,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.example.firebase_config.databinding.SelectClothesToExchangeFragmentBinding
 import com.example.firebase_config.viewmodels.ExchangeViewModel
+import com.example.firebase_config.views.ExchangeClothesActivity
 
 class SelectClothesToExchangeFragment: Fragment() {
 
@@ -28,12 +29,23 @@ class SelectClothesToExchangeFragment: Fragment() {
         viewModel.downloadPosts()
 
         viewModel.userPosts.observe(viewLifecycleOwner){posts ->
-            val adapter = PostAdapterFeed(posts)
-            binding.exchangeRecyclerView.adapter = adapter
-            adapter.notifyDataSetChanged()
+            if(posts.isEmpty()){
+                val exchangeClothesActivity = activity as ExchangeClothesActivity
+                exchangeClothesActivity.loadFragment(exchangeClothesActivity.noClothesToExchangeFragment)
+            }else{
+                val adapter = PostAdapterFeed(posts)
+                binding.exchangeRecyclerView.adapter = adapter
+                adapter.notifyDataSetChanged()
+            }
         }
 
         binding.fpRequiredTV.text = viewModel.getFashionPoints().toString()
+
+        binding.exchangeInfoBtn.setOnClickListener{
+
+            val exchangeClothesActivity = activity as ExchangeClothesActivity
+            exchangeClothesActivity.loadFragment(exchangeClothesActivity.howExchangeWorksFragment)
+        }
 
         return binding.root
     }
